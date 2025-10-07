64af01574209627c62cecf3f8198a5bd494c57e6
Rahul Addagatla <68944730+Rahul-Addagatla@users.noreply.github.com>
2025-10-07 11:51:57 +0530

    Create 1.ingest_csv_files.py

---
+++ scripts/bronze/1.ingest_csv_files.py
@@ -0,0 +1,94 @@
+# Databricks notebook source
+# MAGIC %run "../includes/configuration"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Ingest circuits.csv file
+
+# COMMAND ----------
+
+# Import required data types for defining a custom schema
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DoubleType, DateType
+
+# COMMAND ----------
+
+# Define the schema explicitly for the circuits data
+# This avoids relying on schema inference and ensures data consistency
+circuits_schema = StructType(fields=[StructField("circuitId", IntegerType(), False),
+                                     StructField("circuitRef", StringType(), True),
+                                     StructField("name", StringType(), True),
+                                     StructField("location", StringType(), True),
+                                     StructField("country", StringType(), True),
+                                     StructField("lat", DoubleType(), True),
+                                     StructField("lng", DoubleType(), True),
+                                     StructField("alt", IntegerType(), True),
+                                     StructField("url", StringType(), True)
+])
+
+# COMMAND ----------
+
+# Read the circuits.csv file using the defined schema
+# - `.option("header", True)` tells Spark to use the first line as headers
+# - `raw_folder_path` is defined in the configuration notebook
+circuits_df = spark.read \
+.option("header", True) \
+.schema(circuits_schema) \
+.csv(f"{bronze_folder_path}/circuits.csv")
+
+# COMMAND ----------
+
+circuits_df.write.mode("overwrite").format("delta").saveAsTable("bronze.circuits")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Ingest races.csv file
+
+# COMMAND ----------
+
+races_schema = StructType(fields=[StructField("raceId", IntegerType(), False),
+                                  StructField("year", IntegerType(), True),
+                                  StructField("round", IntegerType(), True),
+                                  StructField("circuitId", IntegerType(), True),
+                                  StructField("name", StringType(), True),
+                                  StructField("date", DateType(), True),
+                                  StructField("time", StringType(), True),
+                                  StructField("url", StringType(), True) 
+])
+
+# COMMAND ----------
+
+races_df = spark.read \
+.option("header", True) \
+.schema(races_schema) \
+.csv(f"{bronze_folder_path}/races.csv")
+
+# COMMAND ----------
+
+races_df.write.mode("overwrite").format("delta").saveAsTable("bronze.races")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Ingest lap_times folder
+
+# COMMAND ----------
+
+lap_times_schema = StructType(fields=[StructField("raceId", IntegerType(), False),
+                                      StructField("driverId", IntegerType(), True),
+                                      StructField("lap", IntegerType(), True),
+                                      StructField("position", IntegerType(), True),
+                                      StructField("time", StringType(), True),
+                                      StructField("milliseconds", IntegerType(), True)
+                                     ])
+
+# COMMAND ----------
+
+lap_times_df = spark.read \
+.schema(lap_times_schema) \
+.csv(f"{raw_folder_path}/lap_times")
+
+# COMMAND ----------
+
+lap_times_df.write.mode("overwrite").format("delta").saveAsTable("bronze.lap_times")
