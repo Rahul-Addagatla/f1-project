0bdb601c1d910cd171dac2f8e39f504f97346b34
Rahul Addagatla <68944730+Rahul-Addagatla@users.noreply.github.com>
2025-10-07 12:51:37 +0530

    Update configuration.py

--- scripts/includes/configuration.py
+++ scripts/includes/configuration.py
@@ -1,4 +1,5 @@
 # Databricks notebook source
+raw_folder_path = "/mnt/formula1dl/raw"
 bronze_folder_path = "/mnt/formula1dl/bronze"
 silver_folder_path = "/mnt/formula1dl/silver"
 gold_folder_path = "/mnt/formula1dl/gold"
