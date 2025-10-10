4e2b61fee3cb679a967febd83fadd8f1cf1d0936
Rahul Addagatla <68944730+Rahul-Addagatla@users.noreply.github.com>
2025-10-10 14:11:42 +0530

    Update 2.driver_standings.py

--- scripts/gold/2.driver_standings.py
+++ scripts/gold/2.driver_standings.py
@@ -12,17 +12,6 @@ race_results_df = spark.read.format("delta").load(f"{presentation_folder_path}/r
 
 # COMMAND ----------
 
-# Extract the list of race years
-race_year_list = df_column_to_list(race_results_df, 'race_year')
-
-# COMMAND ----------
-
-from pyspark.sql.functions import col, sum, when, count, desc, rank, asc
-# Apply the filter
-race_results_df = race_results_df.filter(col("race_year").isin(race_year_list))
-
-# COMMAND ----------
-
 driver_standings_df = race_results_df.groupBy("race_year", "driver_name", "driver_nationality") \
                                      .agg(sum("points").alias("total_points"),
                                      count(when(col("position") == 1, True)).alias("wins"))
